f0480c81d20f5f8a3fc3cf47c61fb6e3cfa5c397
Faza Hijran <hijran.dias13@gmail.com>
2024-12-12 21:37:56 +0700

    menambah tambahan dikit

--- src/js/script.js
+++ src/js/script.js
@@ -18,22 +18,3 @@ hamburger.addEventListener("click", function() {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
 });
-
-// Back To Top
-const scrollToTopButton = document.getElementById("scroll-to-top");
-
-// Show button when user scrolls down
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 100) {
-    scrollToTopButton.style.display = "block";
-  } else {
-    scrollToTopButton.style.display = "none";
-  }
-});
-
-// Smooth scroll to top
-function scrollToTop() {
-  window.scrollTo({
-    top: 0
-  });
-}
